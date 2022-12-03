dc96463ed09eb5300a944d4654221c6da4709447
kizitoomoit <kizitoomoit9@gmail.com>
2022-12-03 14:48:57 +0300

    First commit

---
+++ javascript_function_n_Events/script/main.js
@@ -0,0 +1,5 @@
+/* Creating a function*/
+function changeParagraph() {
+    /* Defining what the function does */
+    document.getElementById("demo").innerHTML = "You have changed, haven't you?.";
+}
